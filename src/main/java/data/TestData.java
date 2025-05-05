8ae3e46bc6256603655ca1ce8d9fc94ae34b92cf
Andrey Kan <andreyevdaaa@yandex.ru>
2025-05-05 16:48:09 +0600

    test: Добавлен класс "TestData", в нем прописаны все данные для тестов и шагов

---
+++ src/main/java/data/TestData.java
@@ -0,0 +1,11 @@
+package data;
+
+public class TestData {
+
+    public static final String BASE_URL = "https://stellarburgers.nomoreparties.site";
+    public static final String NAME = "Testikman";
+    public static final String EMAIL = "testikmail@mail.ru";
+    public static final String PASSWORD = "123456";
+    public static final String INVALID_PASSWORD = "123";
+
+}
